b66017802080da612a8701bdc003cc7885616e6d
shantanu7387 <kulkarnishantanu293@gmail.com>
2024-10-04 09:37:56 +0530

    MAnoj

---
+++ Harsh Manoj Java/StudentClass/src/CLasses/TestStudent.java
@@ -0,0 +1,14 @@
+package CLasses;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int choice =0;
+		do {
+			System.out.println("1)");
+		}while(choice!=4);
+	}
+
+}
