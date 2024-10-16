7cc6426afc3987595e3e2e630ec57a5e2c4d478f
shantanu7387 <kulkarnishantanu293@gmail.com>
2024-10-16 13:55:29 +0530

    file handling

---
+++ Core Java/Day 12/ExceptionDemo/src/com/demo/test/TestException1.java
@@ -0,0 +1,12 @@
+package com.demo.test;
+
+public class TestException1 {
+
+	public static void main(String[] args) {
+		String s=null;
+		s=sc.next();
+		System.out.println(s);
+		System.out.println(s.length());
+	}
+
+}
